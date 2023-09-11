ea05988e1872d12409ae95c4a640e1193ec5ffea
Pedro Moura <pedro.fernandes.moura@gmail.com>
2023-09-11 12:45:57 -0300

    vault backup: 2023-09-11 12:45:57

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,14 +1,16 @@
+# Configuração do backend Azure para armazenamento do estado remoto do Terraform
 terraform {
     backend "azurerm" {
-        resource_group_name = "rg-terraform-backend"
-        storage_account_name = "strecterraformbackend"
-        container_name = "terraform-tfstate"
-        key = "tf-az-#NOME_DO_RECURSO#.tfstate"
+        resource_group_name   = "rg-terraform-backend"            // Nome do grupo de recursos para o backend
+        storage_account_name  = "strecterraformbackend"           // Nome da conta de armazenamento para o backend
+        container_name        = "terraform-tfstate"               // Nome do container de armazenamento
+        key                   = "tf-az-#NOME_DO_RECURSO#.tfstate" // Nome do arquivo de estado remoto
     }
+
     required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.5.0"
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "3.5.0" // Versão requerida do provedor AzureRM
+        }
     }
-  }
 }
