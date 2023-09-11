15757754e18a188f0cd2ddc92fa8bc4e88fa24e1
Pedro Moura <pedro.fernandes.moura@gmail.com>
2023-09-11 09:11:24 -0300

    vault backup: 2023-09-11 09:11:24

--- Terraform/padrao_terraform_azure.tf
+++ Terraform/padrao_terraform_azure.tf
@@ -2,6 +2,12 @@ data "azure_resource_group" "name" {
     name = "nome-do-resource-group"
 }
 
+data "azure_key_vault" "kv-infraestrutura" {
+    name           = nome-keyvout-azure
+    resource_group = nome-do-resourcegroup-da-kv
+}
+
 data "azure_key_vault_secret" "PasswordServerAdmin" {
-    name = "nome-keyvout-azure"
+    name = var.PasswordServerAdmin
+    key_vault_id = data.azure_key_vault.
 }
