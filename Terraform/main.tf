62bdc4f00fcc58987d887216807ddeedf37ecd56
Pedro Moura <pedro.fernandes.moura@gmail.com>
2023-09-11 12:35:56 -0300

    vault backup: 2023-09-11 12:35:56

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,12 @@
 data "azurerm_resource_group" "res-2" {
-  name = "xxx" // <-- Preencher com a resource group que ira fazer deploy
+  name = "xxx" // Preencher com a resource group que ira fazer deploy
 }
 data "azurerm_key_vault" "kv-infraestrutura" {
-  name                = "xxx" // <-- KeyVault name, alterar conforme ambiente
-  resource_group_name = "xxx" // <-- resourceGroup da kv, alterar conforme ambiente
+  name                = "xxx" // KeyVault name, alterar conforme ambiente
+  resource_group_name = "xxx" // resourceGroup da kv, alterar conforme ambiente
 }
 data "azurerm_key_vault_secret" "PasswordServerAdmin" {
-name = var.passwordserveradmin
+name         = var.passwordserveradmin
 key_vault_id = data.azurerm_key_vault.kv-infraestrutura.id
 }
 resource "random_integer" "id" {
@@ -18,7 +18,7 @@ resource "azurerm_windows_virtual_machine" "res-1" {
   admin_password        = data.azurerm_key_vault_secret.PasswordServerAdmin.value  
   license_type          = "Windows_Server"
   location              = data.azurerm_resource_group.res-2.location
-  name                  = "xxx" // <-- Alterar nome da maquina conforme padrão de exemplo (RECAZBRSLDEV001): RECAZ (Recovery Azure) BRS (Localização Brasil South) W (Sistema Operacional) DEV (Ambiente) 001 (Sequencia da maquina)
+  name                  = "xxx" // Alterar nome da maquina conforme padrão de exemplo (RECAZBRSLDEV001): RECAZ (Recovery Azure) BRS (Localização Brasil South) W (Sistema Operacional) DEV (Ambiente) 001 (Sequencia da maquina)
   network_interface_ids = [azurerm_network_interface.res-0.id]
   resource_group_name   = data.azurerm_resource_group.res-2.name
   size                  = var.vm_size
@@ -44,13 +44,13 @@ resource "azurerm_windows_virtual_machine" "res-1" {
 }
 resource "azurerm_network_interface" "res-0" {
   location            = data.azurerm_resource_group.res-2.location
-  name                = format("xxx-%v", random_integer.id.result) // <-- Alterar somente o nome da maquina (xxx)
+  name                = format("xxx-%v", random_integer.id.result) // Alterar somente o nome da maquina (xxx)
   resource_group_name = data.azurerm_resource_group.res-2.name
   dns_servers         = var.dns_servers 
   ip_configuration {
     name                          = "ipconfig1"
     private_ip_address_allocation = var.private_ip_address_allocation
-    subnet_id                     = "xxx" // <-- Preencher essa informação conforme vnet e snet
+    subnet_id                     = "xxx" // Preencher essa informação conforme vnet e snet
   }
   depends_on = [
     data.azurerm_resource_group.res-2,
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,29 +1,32 @@
 variable "admin_username" {
   type        = string
-  default = "recadmin"
+  default = "recadmin" // Nome de usuário do administrador da VM
 }
 
 variable "vm_size" {
   type        = string
-  default     = "Standard_B4ms" // --> Disco usado na vm
+  default = "Standard_B4ms" // Tamanho da VM que será criada (disco usado na VM)
 }
 
 variable "storage_account_type" {
   type        = string
-  default     = "StandardSSD_LRS"
+  default = "StandardSSD_LRS" // Tipo de conta de armazenamento para os discos
 }
 
 variable "private_ip_address_allocation" {
   type        = string
-  default     = "Dynamic"
+  default = "Dynamic" // Alocação de endereço IP privado (Dynamic ou Static)
 }
 
 variable "passwordserveradmin" {
   type        = string
-  default     = "PasswordServerAdmin"
+  default = "PasswordServerAdmin" // Senha para o administrador do servidor
 }
 
 variable "dns_servers" {
   type        = list(string)
-  default     = ["xxx","xxx"]
+  default     = [
+    "xxx",
+    "xxx"
+  ] // Servidores DNS a serem usados pela VM
 }
